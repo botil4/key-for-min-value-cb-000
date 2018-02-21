8e2e7c3ee90db1e20f6a0b2a3b89636397269d6d
Travis Clarkson <tk1667@gmail.com>
2018-02-21 01:59:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,11 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
+    if lowest == nil
+      lowest = key
+    else
     lowest = key if key < lowest
+    end
   end
   lowest
 end
